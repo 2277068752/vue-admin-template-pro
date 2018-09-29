9150b016450fa6cbb044bff138f285b1e9b2b8c6
aegis <changjun@aegis-data.com>
2018-09-29 11:14:40 +0800

    :tada: 初始化项目

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-admin-element-pro
+
+> 个人维护的管理平台，如有改善，敬请fork
---
+++ src/views/index/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="index-page">
+
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data () {
+    return {}
+  },
+  created () {
+  },
+  mounted () {
+  },
+  computed: {},
+  methods: {}
+}
+</script>
+
+<style scoped>
+.index-page {
+  background-color: #fff;
+}
+</style>
